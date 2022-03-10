292842e66076f79a4ce14c42c4a8994dc46a12e3
rangerbd <dipto.g1@gmail.com>
2022-03-10 12:05:44 +0600

    adding Li

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import playerData from './data/data.json';
 
 function App() {
+
+const [players ,setPlayers] =useState([]);
+
+useEffect( () => {
+  setPlayers(playerData);
+  console.log(playerData)
+},[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Total player : {players.length}</h1>
+
+      <ul>
+        {
+          players.map(player => <li>{player.name}</li>)
+        }
+      </ul>
     </div>
   );
 }
